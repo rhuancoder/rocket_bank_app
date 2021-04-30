a41ca6796491e231b3da25084f32a09068427e11
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-04-30 19:39:00 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text("Hello World", textDirection: TextDirection.ltr,));
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            'Main Column - First Text',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Main Column - Second Text',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Main Column - Third Text',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: <Widget>[
+              Text(
+                'Secondary Column - First Text',
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
