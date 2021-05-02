6425ecaa4ece242feb1635a20c755eb88984d87d
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-05-02 19:48:01 -0300

    Changing screen;

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text(
-            'Main Column - First Text',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Main Column - Second Text',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Main Column - Third Text',
-            textDirection: TextDirection.ltr,
-          ),
-          Column(
-            children: <Widget>[
-              Text(
-                'Secondary Column - First Text',
-                textDirection: TextDirection.ltr,
-              ),
-            ],
-          ),
-        ],
+void main() => runApp(MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Transfers'),
       ),
-    );
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    )));
