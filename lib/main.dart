5f2637a47a8a690b687fe2b9cbaa35cd662dad4a
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-05-03 19:30:31 -0300

    Adding cards;

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(
+      MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('Transfers'),
+          body: Column(
+            children: [
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('100.00'),
+                  subtitle: Text('1000'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('100.00'),
+                  subtitle: Text('1000'),
+                ),
+              ),
+            ],
+          ),
+          appBar: AppBar(
+            title: Text('Transfers'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    )));
+    );
