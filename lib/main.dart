1e05c8f76a6240f84ef7de6fa4c59195f6863524
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-05-04 20:03:07 -0300

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          body: Column(
-            children: [
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100.00'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100.00'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-            ],
-          ),
+          body: TransfersList(),
           appBar: AppBar(
             title: Text('Transfers'),
           ),
@@ -30,3 +13,40 @@ void main() => runApp(
         ),
       ),
     );
+
+class TransfersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransferItem(Transfer(100.00, 1000)),
+        TransferItem(Transfer(200.00, 1001)),
+        TransferItem(Transfer(300.00, 1003)),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+}
