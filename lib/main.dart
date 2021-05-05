fa78156c4a04811af39c776059bb36df1810462c
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-05-05 19:44:21 -0300

    Implementing Create Transfer screen;

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: TransfersList(),
-          appBar: AppBar(
-            title: Text('Transfers'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-          ),
-        ),
+void main() => runApp(RocketBankApp());
+
+class RocketBankApp extends StatelessWidget {
+  const RocketBankApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: TransferForm(),
       ),
     );
+  }
+}
+
+class TransferForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Transfer'),
+      ),
+      body: Text('test'),
+    );
+  }
+}
 
 class TransfersList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TransferItem(Transfer(100.00, 1000)),
-        TransferItem(Transfer(200.00, 1001)),
-        TransferItem(Transfer(300.00, 1003)),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transfers'),
+      ),
+      body: Column(
+        children: [
+          TransferItem(Transfer(100.00, 1000)),
+          TransferItem(Transfer(200.00, 1001)),
+          TransferItem(Transfer(300.00, 1003)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
